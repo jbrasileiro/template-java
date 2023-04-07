b1df219b575e975a9871cd484a491fd036841303
João Carlos Brasileiro <->
2023-04-07 11:22:18 -0300

    add default files

---
+++ src/test/java/com/github/jbrasileiro/ms/MicroserviceTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.jbrasileiro.ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
